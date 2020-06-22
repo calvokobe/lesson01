44469a7dbf11d955ac2ca4f6af735d2fe31ade04
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-06-22 16:26:26 +0900

    コード修正しました！

--- 3-4/main.js
+++ 3-4/main.js
@@ -1,15 +1,12 @@
-onst arg1 = String(process.argv[2])
-const arg2 = String(process.argv[3])
 
-const str1 = arg1
-const str2 = arg2
+const str1 = String(process.argv[2])
+const str2 = String(process.argv[3])
 
-const result1 = str1.substr( 2, 1 );
-const result2 = str1.substr( 1, 1 );
-const result3 = str1.substr( 0, 1 );
+function reverseString(str1, str2) {
+    return str1.split("").reverse().join("");
+    return str2.split("").reverse().join("");
+  }
+reverseString(str1);
+reverseString(str2);
 
-const result4 = str2.substr( 2, 1 );
-const result5 = str2.substr( 1, 1 );
-const result6 = str2.substr( 0, 1 );
-
-console.log(Number(result1+result2+result3) + Number(result4+result5+result6));
+console.log(Number(reverseString(str1))+Number(reverseString(str2)));
