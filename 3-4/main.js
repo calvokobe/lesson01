909a43da37e30e52cf11b4d18ea5c5662e6a5213
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-07-02 16:50:43 +0900

    修正掛けました！

--- 3-4/main.js
+++ 3-4/main.js
@@ -1,12 +1,11 @@
-
 const str1 = String(process.argv[2])
 const str2 = String(process.argv[3])
 
 function reverseString(str1, str2) {
-    return str1.split("").reverse().join("");
-    return str2.split("").reverse().join("");
-  }
+  return str1.split("").reverse().join("");
+  return str2.split("").reverse().join("");
+}
 reverseString(str1);
 reverseString(str2);
 
-console.log(Number(reverseString(str1))+Number(reverseString(str2)));
+console.log(Number(reverseString(str1)) + Number(reverseString(str2)));
