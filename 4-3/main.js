113e0f32aaac36ebe2104bad85eb884e57f87bd4
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-07-08 19:01:02 +0900

    4−3、再度やり直しました。 レビューよろしくお願いいたします。

--- 4-3/main.js
+++ 4-3/main.js
@@ -1,5 +1,12 @@
+
 const errorLogs = [
   "Error 101:Invalid Exception AAA - 2020/01/01/01:00",
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
 ]
+
+for (let i = 0; i < 3; i++){
+  const first  = errorLogs[i].indexOf(':');
+  const second  = errorLogs[i].indexOf('-');
+  console.log(errorLogs[i].substring(first +1, second))
+};
