6759a7d7c88771a0d61fbf4be861d3789e04ec67
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-07-08 18:53:28 +0900

    インデント修正かけましたが、反映されていますでしょうか？ レビュー、よろしくお願いいたします。

--- 4-2/main.js
+++ 4-2/main.js
@@ -1,8 +1,8 @@
-for (let i = 0; i<=50; i++){
+for (let i = 0; i <= 50; i++) {
     const str = i.toString()
-    if ((i % 3) == 0 || str.match(/3/)){ 
-    console.log("WOW");
+    if ((i % 3) == 0 || str.match(/3/)) {
+        console.log("WOW");
     } else {
-    console.log(i);
+        console.log(i);
     }
 }
