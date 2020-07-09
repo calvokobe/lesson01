610e82063aa84f1e39d2b8e1531673fe68ba18d2
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-07-09 09:54:58 +0900

    「季節を表示する」を実装しました、レビューよろしくお願いいたします。

---
+++ 2-4/main.js
@@ -0,0 +1,10 @@
+const num = Number(process.argv[2])
+if (num > 1 && num < 5) {
+    console.log("春");
+} else if (num > 4 && num < 8) {
+    console.log("夏");
+} else if (num > 7 && num < 11) {
+    console.log("秋");
+} else {
+    console.log("冬")
+}  
