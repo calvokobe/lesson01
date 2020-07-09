636b8f72887ae64e21b4af473641a863491bad5b
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-07-09 09:32:33 +0900

    「探索」を実装しました、レビューよろしくお願いいたします。

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,13 @@
+let counts1 = 0
+let counts2 = 0
 const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+for (let a = 0; a < str.length; a++) {
+    if (str[a] === 'こ' && str[a + 1] === 'は' && str[a + 2] === 'る') {
+        counts1++;
+    }
+    if (str[a] === 'は' && str[a + 1] === 'る' && str[a + 2] === 'こ') {
+        counts2++;
+    }
+}
+console.log(`こはる:${counts1}`);
+console.log(`はるこ:${counts2}`);
