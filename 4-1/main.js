c0ada5946f46c424d92072673622404a90b2409b
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-07-14 13:38:04 +0900

    「中間値」のコード修正を行いました、再レビューをどうぞよろしくお願いいたします。

--- 4-1/main.js
+++ 4-1/main.js
@@ -1,9 +1,7 @@
-const arg1 = Number(process.argv[2])
-const arg2 = Number(process.argv[3])
-const arg3 = Number(process.argv[4])
-const arg4 = Number(process.argv[5])
-const arg5 = Number(process.argv[6])
-
-let array = [arg1, arg2, arg3, arg4, arg5]
-array.sort( (a,b) =>  a-b  );
-console.log(array[2])
+const math = require("mathjs");
+const array = []
+for (let i = 2; i < process.argv.length; i++) {
+    array.push(process.argv[i])
+}
+const result = math.median([array]);
+console.log(result)
