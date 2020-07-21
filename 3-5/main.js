49aa1c7eb011c9824544a716cb9a1709b0d99bab
Keiji_Matsumoto <calvo.kobe@gmail.com>
2020-07-21 13:42:58 +0900

    「分布表」を書き直しました、再度のレビューをどうぞよろしくお願いいたします。

--- 3-5/main.js
+++ 3-5/main.js
@@ -1,39 +1,17 @@
-const students = [17,38,100,95,23,62,77,45,69,81,83,51,42,36,60]
+const students = [17, 38, 100, 95, 23, 62, 77, 45, 69, 81, 83, 51, 42, 36, 60]
 const array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-for (let i=0; i<students.length; i++){
-if (students[i] > 0 && students[i] < 10){
-    array[0]++
-} else if (students[i] >= 10 && students[i] < 20){
-    array[1]++
-} else if (students[i] >= 20 && students[i] < 30){
-    array[2]++
-} else if (students[i] >= 30 && students[i] < 40){
-    array[3]++
-} else if (students[i] >= 40 && students[i] < 50){
-    array[4]++
-} else if (students[i] >= 50 && students[i] < 60){
-    array[5]++
-} else if (students[i] >= 60 && students[i] < 70){
-    array[6]++
-} else if (students[i] >= 70 && students[i] < 80){
-    array[7]++
-} else if (students[i] >= 80 && students[i] < 90){
-    array[8]++
-} else if (students[i] >= 90 && students[i] < 100){
-    array[9]++
-} else {
-    array[10]++
-}
+for (let i = 0; i < students.length; i++) {
+    array[Math.floor(students[i] / 10)]++
 }
 
-for (let j=3; j>0; j--){
+for (let j = Math.max(...array); j > 0; j--) {
     const box = []
-    for (let i=0; i<array.length; i++){     
-        if (array[i] >= j){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= j) {
             box.push("*");
-        } else {    
+        } else {
             box.push(" ");
-        } 
+        }
     }
     console.log(box.join("  "));
 }
